f34f22efdd66576e74f8d5d9007568b07d7444c1
Your Name <you@example.com>
2024-12-08 12:44:10 +0900

    match

--- webapp/go/matching.go
+++ webapp/go/matching.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"log/slog"
 )
 
 func runMatching() {
@@ -12,8 +13,10 @@ func runMatching() {
 	ride := &Ride{}
 	if err := db.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1`); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
+			slog.Info("no rides")
 			return
 		}
+		slog.Info("match error 1")
 		return
 	}
 
@@ -22,22 +25,27 @@ func runMatching() {
 	for i := 0; i < 10; i++ {
 		if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
 			if errors.Is(err, sql.ErrNoRows) {
+				slog.Info("no chairs")
 				return
 			}
 		}
 
 		if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
+			slog.Info("match error 2")
 			return
 		}
 		if empty {
+			slog.Info("empty")
 			break
 		}
 	}
 	if !empty {
+		slog.Info("not empty")
 		return
 	}
 
 	if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
+		slog.Info("failed to update ride")
 		return
 	}
 
