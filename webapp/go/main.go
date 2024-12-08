b3d30fffa19ee5e99c5b120f85744d862359dbe4
chibiegg <chibiegg@chibiegg.net>
2024-12-08 14:49:39 +0900

    builk

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -122,23 +122,35 @@ func setup() http.Handler {
 				chairLocationCacheMapRWMutex.Lock()
 				defer chairLocationCacheMapRWMutex.Unlock()
 
+				data := []map[string]interface{}{}
+
 				for _, cll := range chairLocationCacheMap {
 					if cll.isDirty { // now < cll.UpdatedAt
-						// 更新されているのでDBに保存する
-						if _, err := tx.ExecContext(
-							ctx,
-							`INSERT INTO chair_locations_latest (chair_id, latitude, longitude, updated_at, total_distance) VALUES (?, ?, ?, ?, ?)
-							ON DUPLICATE KEY UPDATE 
-								latitude = ?, longitude = ?, updated_at = ?, total_distance = ?`,
-							cll.ChairID, cll.Latitude, cll.Longitude, cll.UpdatedAt, cll.TotalDistance, cll.Latitude, cll.Longitude, cll.UpdatedAt, cll.TotalDistance,
-						); err != nil {
-							slog.Error("failed to insert chair location", "error", err)
-						}
+						data = append(data, map[string]interface{}{
+							"chair_id": cll.ChairID,
+							"latitude": cll.Latitude,
+							"longitude": cll.Longitude,
+							"updated_at": cll.UpdatedAt,
+							"total_distance": cll.TotalDistance,
+						})
 						cll.isDirty = false
 						slog.Info("saved chair location", "chair_id", cll.ChairID)
 					}
 				}
-			
+
+				if len(data) == 0 {
+					// 更新されているのでDBに保存する
+					if _, err := tx.ExecContext(
+						ctx,
+						`INSERT INTO chair_locations_latest (chair_id, latitude, longitude, updated_at, total_distance) VALUES (:chair_id, :latitude, :longitude, :updated_at, :total_distance)
+						ON DUPLICATE KEY UPDATE latitude = :latitude, longitude = :longitude, updated_at = :updated_at, total_distance = :total_distance`,
+						data,
+					); err != nil {
+						slog.Error("failed to insert chair location", "error", err)
+					}
+					slog.Info("saved chair locations", "count", len(data))
+				}
+				
 			}()
 		}
 	}()
