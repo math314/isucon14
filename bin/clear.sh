54520710e8ec46291a1a87f92095e469ab730a9f
Server01 <chibiegg@chibiegg.net>
2024-12-08 02:01:09 +0000

    スクリプト

---
+++ bin/clear.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -xe
+
+sudo rm /var/log/nginx/access.log
+sudo service nginx reload
+
+
--- bin/log.sh
+++ bin/log.sh
@@ -4,6 +4,11 @@ TIME=`TZ=Asia/Tokyo date +"%I%M%S"`
 NOTE="$1"
 VERSION=`bash -c "cd /home/isucon; git describe --dirty --always --tags"`
 
+if [ -z "${NOTE}" ];then
+	echo "argument not found"
+	exit 1
+fi
+
 FILENAME="${TIME}_${NOTE}_${VERSION}.log"
 
 sudo mv -v /var/log/nginx/access.log "/var/log/nginx/${FILENAME}"
