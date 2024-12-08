8ccd6d8b63381d25a1469eb013bfad9ec8ae3337
chibiegg <chibiegg@chibiegg.net>
2024-12-08 12:53:27 +0900

    RetryAfterMsを環境変数に

--- webapp/go/chair_handlers.go
+++ webapp/go/chair_handlers.go
@@ -199,7 +199,7 @@ func chairGetNotification(w http.ResponseWriter, r *http.Request) {
 	if err := tx.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id = ? ORDER BY updated_at DESC LIMIT 1`, chair.ID); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			writeJSON(w, http.StatusOK, &chairGetNotificationResponse{
-				RetryAfterMs: 500,
+				RetryAfterMs: chairRetryAfterMs,
 			})
 			return
 		}
@@ -259,7 +259,7 @@ func chairGetNotification(w http.ResponseWriter, r *http.Request) {
 			},
 			Status: status,
 		},
-		RetryAfterMs: 500,
+		RetryAfterMs: chairRetryAfterMs,
 	})
 }
 
