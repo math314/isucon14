9c85e4aa27cf30e64ef6e0e8d16a2c62a7a95641
Server01 <chibiegg@chibiegg.net>
2024-12-08 01:49:11 +0000

    update get-slowlog

--- webapp/get-slowlog.sh
+++ webapp/get-slowlog.sh
@@ -1,4 +1,4 @@
-FILENAME=/home/isucon/mysql-slow-$(date +%Y%m%d_%H%M%S).log
+FILENAME=/home/isucon/logs/mysql-slow-$(date +%Y%m%d_%H%M%S).log
 
 sudo cp /var/log/mysql/mysql-slow.log $FILENAME
 sudo chown isucon:isucon $FILENAME
