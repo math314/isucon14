8788658f65d788c0b90b94227263c8f0454f03d6
Yosuke Sameshima <sameshima@monoxer.com>
2025-01-03 11:50:05 +0900

    remove transaction

--- webapp/go/chair.go
+++ webapp/go/chair.go
@@ -11,27 +11,17 @@ var ErrNoChairs = fmt.Errorf("no chairs")
 func getChairNotification(ctx context.Context, chair *Chair) (*chairGetNotificationResponseData, error) {
 	nextData, newNotification := takeLatestUnsentNotificationResponseDataToChair(chair.ID)
 
-	tx, err := db.Beginx()
-	if err != nil {
-		return nil, err
-	}
-	defer tx.Rollback()
-
 	if nextData == nil {
 		return nil, ErrNoChairs
 	}
 
 	if newNotification && nextData.Status == "COMPLETED" {
-		if _, err := tx.ExecContext(
+		if _, err := db.ExecContext(
 			ctx,
 			`UPDATE chairs SET is_free = TRUE WHERE id = (SELECT chair_id FROM rides WHERE id = ?)`,
 			nextData.RideID); err != nil {
 			return nil, err
 		}
-	}
-
-	if err := tx.Commit(); err != nil {
-		return nil, err
 	}
 
 	if newNotification {
