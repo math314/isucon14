f179d0605298e4873400485e86e8218880081749
Yosuke Sameshima <sameshima@monoxer.com>
2025-01-03 23:36:18 +0900

    use second server as app

--- deploy/deploy2.sh
+++ deploy/deploy2.sh
@@ -5,14 +5,14 @@ cd $(dirname $0)
 source ~/.local.env
 
 # # go service
-# cd ../webapp/go
-# echo `pwd`
-# echo `which go`
-# go build -o isuride
-# cd ../../deploy
-# sudo systemctl stop isuride-go.service
-# cp ../env2.sh /home/isucon/env.sh
-# sudo systemctl restart isuride-go.service
+cd ../webapp/go
+echo `pwd`
+echo `which go`
+go build -o isuride
+cd ../../deploy
+sudo systemctl stop isuride-go.service
+cp ../env2.sh /home/isucon/env.sh
+sudo systemctl restart isuride-go.service
 
 # # nginx service
 # sudo cp ../webapp/config/nginx02.conf /etc/nginx/nginx.conf
