e3ad8a6fb9f549cb84000f934000e59c146f92a0
Yosuke Sameshima <sameshima@monoxer.com>
2025-01-05 22:18:28 +0900

    in memory

--- webapp/go/ride.go
+++ webapp/go/ride.go
@@ -48,17 +48,17 @@ func checkStatusAndUpdateChairFreeFlag(ctx context.Context, request RideStatusSe
 	}
 	rideStatusSentAt := rideStatusSentAtCache[request.RideStatusID]
 
-	rideStatus := RideStatus{}
-	if err := db.GetContext(ctx, &rideStatus, `SELECT * FROM ride_statuses WHERE id = ?`, request.RideStatusID); err != nil {
-		return err
-	}
+	// rideStatus := RideStatus{}
+	// if err := db.GetContext(ctx, &rideStatus, `SELECT * FROM ride_statuses WHERE id = ?`, request.RideStatusID); err != nil {
+	// 	return err
+	// }
 
-	if rideStatusSentAt.AppNotificationDone != (rideStatus.AppSentAt != nil) {
-		slog.Error("appSentAt is not consistent", "rideStatus", rideStatus, "rideStatusSentAt", rideStatusSentAt)
-	}
-	if rideStatusSentAt.ChairNotificationDone != (rideStatus.ChairSentAt != nil) {
-		slog.Error("chairSentAt is not consistent", "rideStatus", rideStatus, "rideStatusSentAt", rideStatusSentAt)
-	}
+	// if rideStatusSentAt.AppNotificationDone != (rideStatus.AppSentAt != nil) {
+	// 	slog.Error("appSentAt is not consistent", "rideStatus", rideStatus, "rideStatusSentAt", rideStatusSentAt)
+	// }
+	// if rideStatusSentAt.ChairNotificationDone != (rideStatus.ChairSentAt != nil) {
+	// 	slog.Error("chairSentAt is not consistent", "rideStatus", rideStatus, "rideStatusSentAt", rideStatusSentAt)
+	// }
 
 	// slog.Info("checkStatusAndUpdateChairFreeFlag", "rideStatus", rideStatus)
 
@@ -77,9 +77,9 @@ func checkStatusAndUpdateChairFreeFlag(ctx context.Context, request RideStatusSe
 
 func updateRideStatusAppSentAt(ctx context.Context, request RideStatusSentAtRequest) (time.Time, error) {
 	time := time.Now()
-	if _, err := db.ExecContext(ctx, `UPDATE ride_statuses SET app_sent_at = ? WHERE id = ?`, time, request.RideStatusID); err != nil {
-		return time, err
-	}
+	// if _, err := db.ExecContext(ctx, `UPDATE ride_statuses SET app_sent_at = ? WHERE id = ?`, time, request.RideStatusID); err != nil {
+	// 	return time, err
+	// }
 	if rideStatusSentAtCache[request.RideStatusID] == nil {
 		rideStatusSentAtCache[request.RideStatusID] = &RideStatusSentAt{}
 	}
@@ -99,9 +99,9 @@ func updateRideStatusAppSentAt(ctx context.Context, request RideStatusSentAtRequ
 
 func updateRideStatusChairSentAt(ctx context.Context, request RideStatusSentAtRequest) (time.Time, error) {
 	time := time.Now()
-	if _, err := db.ExecContext(ctx, `UPDATE ride_statuses SET chair_sent_at = ? WHERE id = ?`, time, request.RideStatusID); err != nil {
-		return time, err
-	}
+	// if _, err := db.ExecContext(ctx, `UPDATE ride_statuses SET chair_sent_at = ? WHERE id = ?`, time, request.RideStatusID); err != nil {
+	// 	return time, err
+	// }
 	if rideStatusSentAtCache[request.RideStatusID] == nil {
 		rideStatusSentAtCache[request.RideStatusID] = &RideStatusSentAt{}
 	}
