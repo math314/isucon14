b5d5a3e96111fe940cc348b711cd887b223b561a
Yosuke Sameshima <sameshima@monoxer.com>
2025-01-05 12:23:18 +0900

    fast chair sse

--- webapp/go/chair.go
+++ webapp/go/chair.go
@@ -5,17 +5,3 @@ import (
 )
 
 var ErrNoChairs = fmt.Errorf("no chairs")
-
-func getChairNotification(chair *Chair) (*chairGetNotificationResponseData, error) {
-	nextData, newNotification := takeLatestUnsentNotificationResponseDataToChair(chair.ID)
-
-	if nextData == nil {
-		return nil, ErrNoChairs
-	}
-
-	if newNotification {
-		return nextData, nil
-	} else {
-		return nil, nil
-	}
-}
